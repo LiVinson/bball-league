5d59617cebad56b649bdb3792f7d0b1bbd260854
Lisa Vinson <Lrv0828@gmail.com>
2020-04-30 12:21:28 -0400

    outline of Home, Players, Teams components with basic routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Home from "./Home"
+import Players from "./Players"
+import Teams from "./Teams"
+import Navbar from "./Navbar"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Route path="/" exact component={Home} />
+          <Route path="/players" component={Players} />
+          <Route path="/teams" component={Teams} />
+        </div>
+      </Router>
+    )
+  }
 }
 
-export default App;
+export default App
